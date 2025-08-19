d676dff58efcdb41aa9763932174aa7dc5789e18
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-19 21:58:20 +0500

    Static Members

--- hello.ts
+++ hello.ts
@@ -1,23 +1,37 @@
-class User {
-  constructor(
-    public _name: string,
-    public age: number,
-    public gender?: string
-  ) {}
+// Static Variables
 
-  // Getter and Setter
-  get name() {
-    return this._name;
-  }
-  set name(val: string) {
-    this._name = val;
+/*
+ Why ?? 
+
+
+  Eg: We use Math.PI and we get value of PI
+
+     Similarly we use Math.random and we get random value 
+
+     The Question is : We didn't declare any object or instance of Math ( since it's a class )
+
+and let's say we make this class:
+
+                class APP{
+                  version = 1.0
+                }
+
+                Now to get version of app we can't use this:
+
+                APP.version;  // It will give error
+
+                So what we can do here?
+                We can use "static" keyword
+
+like this: 
+*/
+
+class APP {
+  static version = 1.2;
+
+  static getRandomNumber() {
+    return Math.random();
   }
 }
-
-let u1 = new User("Hashir", 21, "male");
-let u2 = new User("king", 44);
-
-u1.name = "OK"; // set name
-
-console.log(u1.name); // get name
-console.log(u2);
+console.log(APP.version); // we can do this
+console.log("Random number: ", APP.getRandomNumber());
