e0d77950c0307ca19597357c98efe719c3ac909c
pikacoder44 <syedhashiralishah1@gmail.com>
2025-08-19 14:12:21 +0500

    Day 5

--- hello.js
+++ hello.js
@@ -1,13 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function abcd(obj) { }
-abcd({
-    name: "Hashir",
-    email: "hashir@gmail.com",
-    password: "12",
-});
-function Abcde(data) {
-    data.age = 20;
-    data.name = "Hashir";
+class BakeryItem {
+    name;
+    category;
+    price;
+    constructor(name, category, price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        console.log("Object Made -> ");
+        console.log("Name: ", this.name);
+        console.log("Price: ", this.price);
+        console.log("Category: ", this.category);
+    }
+    makeBuiscuits() {
+        console.log("1 Buiscuit made");
+    }
 }
+let b1 = new BakeryItem("milk", "Dairy", 160);
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,1 +1,21 @@
-a=1
+class BakeryItem {
+  constructor(
+    public name: string,
+    public category: string,
+    public price: number
+  ) {
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    console.log("Object Made -> ");
+    console.log("Name: ", this.name);
+    console.log("Price: ", this.price);
+    console.log("Category: ", this.category);
+  }
+
+  makeBuiscuits() {
+    console.log("1 Buiscuit made");
+  }
+}
+
+let b1 = new BakeryItem("milk", "Dairy", 160);
