98bfc7b7213d931656665465ef47dfd32cd00e5a
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-19 21:27:20 +0500

    Day 5 - OPP

--- hello.js
+++ hello.js
@@ -2,14 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class User {
     name;
-    constructor(name) {
+    age;
+    gender;
+    constructor(name, age, gender) {
         this.name = name;
+        this.age = age;
+        this.gender = gender;
     }
     changeName() {
         this.name = "Changed";
     }
 }
-let u1 = new User("Hashir");
+// ?: makes the parameter optional 
+let u1 = new User("Hashir", 21, "male");
+let u2 = new User("king", 44);
 // u1.changeName() // Not Allowed
 console.log(u1);
+console.log(u2);
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,11 +1,15 @@
 class User {
-  constructor(public readonly name: string) {}
+  constructor(public name: string, public age:number, public gender?:string) {}
 
   changeName() {
     this.name = "Changed";
   }
 }
 
-let u1 = new User("Hashir");
+// ?: makes the parameter optional 
+
+let u1 = new User("Hashir",21,"male");
+let u2 = new User("king",44)
 // u1.changeName() // Not Allowed
 console.log(u1);
+console.log(u2)
