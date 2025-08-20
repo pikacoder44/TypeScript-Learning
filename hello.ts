cc47be30d6c251d154a1bc1b04bbd016fdf9c800
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-20 21:23:41 +0500

    Rest vs Spread Operators

--- hello.ts
+++ hello.ts
@@ -13,3 +13,16 @@ function add(...args: number[]) {
 }
 
 add(1, 2, 3, 4, 5, 6, 7, 7, 3, 23, 23, 2, 3, 12, 21, 2, 4);
+
+// Rest vs Spread Operator
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [...arr1];
+console.log(arr2); // This will give you [1,2,3,4]
+
+// And when we change something in arr 2
+
+arr2.push(5);
+console.log("After adding an element to Arr2");
+console.log("Array 1: ", arr1); // [1,2,3,4]
+console.log("Array 2: ", arr2); // [1,2,3,4,5]
