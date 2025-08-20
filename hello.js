4b9565cceb9b85d0a327a62497f09bbd42b8311c
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-20 20:18:27 +0500

    Functions 2

--- hello.js
+++ hello.js
@@ -2,9 +2,10 @@
 // Function
 Object.defineProperty(exports, "__esModule", { value: true });
 function User(name, age, cb) {
-    console.log(name, age, cb());
+    console.log(name, age, cb("So Jao"));
 }
-let u1 = User("Hashir", 21, () => {
-    return "Done";
+let u1 = User("Hashir", 21, (msg) => {
+    return msg;
 });
+// Output : Hashir 21 So Jao
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,10 +1,11 @@
 // Function
 
-function User(name: string, age: number, cb: () => string) {
-  console.log(name, age, cb());
+function User(name: string, age: number, cb: (msg: string) => string) {
+  console.log(name, age, cb("So Jao"));
 }
 
-let u1 = User("Hashir", 21, () => {
-  return "Done"
+let u1 = User("Hashir", 21, (msg: string) => {
+  return msg;
 });
 
+// Output : Hashir 21 So Jao
