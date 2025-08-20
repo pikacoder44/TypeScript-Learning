fd662438b79eb5e195343e9d1ec30af625b9a038
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-20 20:13:43 +0500

    Functions

--- hello.js
+++ hello.js
@@ -1,37 +1,10 @@
 "use strict";
-// Abstract Classes
-/*
-  Abstract means unclear ---- Opposite of Concrete
-
-  Eg: We say Payment ( so this is unclear as we dont know which payment method is used here )
-
-  When we say Payment through Easypaisa / JazzCash / Nayapay / Sadapay
-              |
-              |-> Now we are Clear about it
-        
-        So Payment is Abstract and EasyPaisa is the implementation
-  
-
-*/
+// Function
 Object.defineProperty(exports, "__esModule", { value: true });
-// Abstract Class:
-class Payment {
-    amount;
-    account;
-    constructor(amount, account) {
-        this.amount = amount;
-        this.account = account;
-    }
-    isPaymentvalid(amount) {
-        return this.amount > 0;
-    }
+function User(name, age, cb) {
+    console.log(name, age, cb());
 }
-class EasyPaisa extends Payment {
-    constructor(amount, account) {
-        super(amount, account);
-    }
-}
-// let p1 = new Payment() // Can't do this 
-let ep1 = new EasyPaisa(20000, 1234);
-console.log(ep1);
+let u1 = User("Hashir", 21, () => {
+    return "Done";
+});
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,38 +1,10 @@
-// Abstract Classes
-/*
-  Abstract means unclear ---- Opposite of Concrete
+// Function
 
-  Eg: We say Payment ( so this is unclear as we dont know which payment method is used here )
-
-  When we say Payment through Easypaisa / JazzCash / Nayapay / Sadapay 
-              |
-              |-> Now we are Clear about it
-        
-        So Payment is Abstract and EasyPaisa is the implementation
-  
-
-*/
-
-// Abstract Class:
-abstract class Payment {
-  constructor(protected amount: number, protected account: number) {}
-
-  isPaymentvalid(amount: number) {
-    return this.amount > 0;
-  }
+function User(name: string, age: number, cb: () => string) {
+  console.log(name, age, cb());
 }
 
+let u1 = User("Hashir", 21, () => {
+  return "Done"
+});
 
-class EasyPaisa extends Payment{
-
-  constructor(amount:number, account:number){
-    super(amount,account)
-  }
-
-}
-
-
-// let p1 = new Payment() // Can't do this 
-
-let ep1 = new EasyPaisa(20000,1234)
-console.log(ep1)
