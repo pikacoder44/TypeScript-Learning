b49142f7032c37399c1332ce0097e989a2fe5254
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-21 06:48:03 +0500

    Type Assertion & Casting

--- hello.js
+++ hello.js
@@ -1,17 +1,39 @@
 "use strict";
 /*
 
-   ------------------------------------- Generic Classes -----------------------------------------------
+   ------------------------------------- Type Assertion -----------------------------------------------
+   
+   Type asserion is used when you know type of value/var more than TS
 
-
-*/
+   How do we defin that:
+   */
 Object.defineProperty(exports, "__esModule", { value: true });
-class BottleMaker {
-    constructor(key) {
-        console.log("Key: ", key);
-        console.log("Type of key: ", typeof key);
-    }
-}
-let b1 = new BottleMaker("ok");
-let b2 = new BottleMaker(2);
+let a = 12;
+a;
+a;
+// or
+let str = a;
+let num = a;
+/*
+------------------------------------- Type Casting -----------------------------------------------
+
+            Basically converting types of vars
+*/
+let stringed = "12";
+let num1 = Number(stringed);
+console.log(typeof num1); //  number
+let hello = 12;
+let helloed = String(hello);
+console.log(typeof helloed); //  String
+/*
+------------------------------------- Non-null asertion operator -----------------------------------------------
+
+            hum variable ke agy ( ! ) wala sign laga dete hen
+            Jiske bare me hum 100% sure hote hen ke yeh var
+            Null or Undefined to Bilkul nahi hai
+*/
+let l;
+l = "hey";
+let char = l.charAt(1);
+console.log(char); //   e
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,16 +1,49 @@
 /*
 
-   ------------------------------------- Generic Classes -----------------------------------------------
+   ------------------------------------- Type Assertion -----------------------------------------------
+   
+   Type asserion is used when you know type of value/var more than TS
 
+   How do we defin that:
+   */
 
+let a: any = 12;
+
+a as string;
+a as number;
+
+// or
+
+let str = <string>a;
+let num = <number>a;
+
+/*
+------------------------------------- Type Casting -----------------------------------------------
+
+            Basically converting types of vars 
 */
 
-class BottleMaker<T> {
-  constructor(key: T) {
-    console.log("Key: ", key);
-    console.log("Type of key: ", typeof key);
-  }
-}
+let stringed = "12";
+let num1 = Number(stringed);
 
-let b1 = new BottleMaker<string>("ok");
-let b2 = new BottleMaker<number>(2);
+console.log(typeof num1); //  number
+
+let hello = 12;
+let helloed = String(hello);
+
+console.log(typeof helloed); //  String
+/*
+------------------------------------- Non-null asertion operator -----------------------------------------------
+
+            hum variable ke agy ( ! ) wala sign laga dete hen
+            Jiske bare me hum 100% sure hote hen ke yeh var
+            Null or Undefined to Bilkul nahi hai 
+*/
+
+let l: null | undefined | string;
+l = "hey";
+
+let char = l!.charAt(1);
+
+console.log(char); //   e
+
