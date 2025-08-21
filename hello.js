c7d067ce22d0bbf7b9c4cc770309c4817e8f1087
Syed Hashir Ali <pikaop125@gmail.com>
2025-08-21 06:22:57 +0500

    Generic Interfaces

--- hello.js
+++ hello.js
@@ -1,48 +1,17 @@
 "use strict";
 /*
 
-   ------------------------------------- Generics -----------------------------------------------
-
-   
-   
-   Genaric Functions
-   Generic Interfaces
-   Generic Classes
-   
-   
-   
-   We want to create a function that will acept any value and print it
-
-   function logger(a:any){
-    console.log(a)
-  }
-  
-  logger("ok")
-  logger(2)
-logger(true)
-
-We can't do that, because if you have to make type of a any, then the reason of using typescript is dead and we can't use any function like a.splice etc coz it's type is not defined
+   ------------------------------------- Generic Interfaces -----------------------------------------------
 
 
 */
 Object.defineProperty(exports, "__esModule", { value: true });
-/*
-
-So what we can do?
-We can tell TS how to treat arguments of function ( types of args ) while using that function
-
-
-*/
-function logger(a) {
-    // T is template name it can be anything like H, K , L etc
-    console.log(a);
+function abcd(obj) {
+    console.log(obj);
 }
-logger("ok");
-logger(22);
-logger(true);
-logger(null);
-// and this <string> , <number>   is not necessary, TS can infer based on data
-logger(undefined);
-logger("Logged String");
-// These are also fine ^
+abcd({
+    name: "Supra",
+    price: 10000000,
+    key: "supra-2025",
+});
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,50 +1,22 @@
 /*
 
-   ------------------------------------- Generics -----------------------------------------------
-
-   
-   
-   Genaric Functions
-   Generic Interfaces
-   Generic Classes
-   
-   
-   
-   We want to create a function that will acept any value and print it
-
-   function logger(a:any){
-    console.log(a)
-  }
-  
-  logger("ok")
-  logger(2)
-logger(true)
-
-We can't do that, because if you have to make type of a any, then the reason of using typescript is dead and we can't use any function like a.splice etc coz it's type is not defined
+   ------------------------------------- Generic Interfaces -----------------------------------------------
 
 
 */
 
-/*
-
-So what we can do?
-We can tell TS how to treat arguments of function ( types of args ) while using that function
-
-
-*/
-function logger<T>(a: T) {
-  // T is template name it can be anything like H, K , L etc
-  console.log(a);
+interface Car<T> {
+  name: string;
+  price: number;
+  key: T;
 }
 
-logger<string>("ok");
-logger<number>(22);
-logger<boolean>(true);
-logger<null>(null);
+function abcd(obj: Car<string>) {
+  console.log(obj);
+}
 
-// and this <string> , <number>   is not necessary, TS can infer based on data
-
-logger(undefined);
-logger("Logged String");
-
-// These are also fine ^
+abcd({
+  name: "Supra",
+  price: 10000000,
+  key: "supra-2025",
+});
